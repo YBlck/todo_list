e6b65b6d7117743cd3ac5c6e5380966204974025
Yura Chornyi <chorny.yura@gmail.com>
2025-03-18 02:55:29 +0200

    feat: add base views

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from manager.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("manager:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("manager:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
